2163b64650f2e14e91e20f1d454a09752f126d4a
Hikmahx <hikmayousuph@gmail.com>
2022-01-12 21:03:09 +0100

    competitions and competition components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Competitions from "./components/Competitions";
 
 function App() {
   return (
     <div className="App min-h-screen ">
       <Header />
+      <Competitions />
     </div>
   );
 }
